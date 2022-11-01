34a3e2c0bb22f6bd9b7b23fbd94488a260c1d11e
shockValue666 <mapeDeveloper@protonmail.com>
2022-11-01 22:33:32 +0200

    fdsadsf

--- src/utils/connectdb.ts
+++ src/utils/connectdb.ts
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseId = process.env.NEXT_PUBLIC_SID
 
-export const supabase = createClient("https://ybmhbwgukqfkksysxbca.supabase.co",supabaseId)
+export const supabase = createClient("https://uswdjukdbouytpnnelxe.supabase.co",supabaseId)
 
